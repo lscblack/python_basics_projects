4f1f24dbf76004b8c0affe88b54359bf022cfd14
lscblack <l.christian@alustudent.com>
2023-11-10 11:16:07 +0200

    Medium Questions

--- easy_medium/Number_Guessing_Game.py
+++ easy_medium/Number_Guessing_Game.py
@@ -1,5 +1,7 @@
 # Design a number guessing game where the computer randomly selects a number, and the user has to guess it within a limited number of attempts.
-import  random
+import random
+
+
 def guess_me(prompt, guess):
     if prompt == guess:
         return "Correct You have Passed"
@@ -7,16 +9,16 @@ def guess_me(prompt, guess):
         return "Incorrect Try Again"
 
 
-guess = random.randint(1,5)
+guess = random.randint(1, 10)
 d = 0
 while True:
-    prompt = int(input(f"Enter Your Guess Range(1-10) left {4-d}/4 : "))
+    prompt = int(input(f"Enter Your Guess Range(1-10) left {5-d}/5 : "))
     if guess_me(prompt=prompt, guess=guess) == "Correct You have Passed":
         print("\033[32m\U0001F604\033[0m", guess_me(prompt=prompt, guess=guess), "\033[32m✓\033[0m ")
         break
     else:
         print("\033[31m\U0001F602\033[0m",guess_me(prompt=prompt, guess=guess), " \033[31mX\033[0m")
-    if d == 3:
+    if d == 5:
         print("End Of Your Tail")
         break
     else:
